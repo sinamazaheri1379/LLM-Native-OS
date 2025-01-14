90c2a32b2403b1662de4f9693384aee775dfac68
sinamazaheri1379 <sinamazaheri1707@gmail.com>
2025-01-14 16:19:03 -0500

    Add Access Manager

---
+++ aios-kernel/include/access_manager.h
@@ -0,0 +1,8 @@
+//
+// Created by sina-mazaheri on 1/14/25.
+//
+
+#ifndef ACCESS_MANAGER_H
+#define ACCESS_MANAGER_H
+
+#endif //ACCESS_MANAGER_H
---
+++ aios-kernel/src/access_manager.c
@@ -0,0 +1,5 @@
+//
+// Created by sina-mazaheri on 1/14/25.
+//
+
+#include "access_manager.h"
